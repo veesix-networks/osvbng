7f34177a79d57e6c092bd1689750916a0e295026
BSpendlove <8687668+BSpendlove@users.noreply.github.com>
2026-01-24 13:45:42 +0000

    Removed old dhcp code when I was testing dhcp proxy

--- pkg/events/topics.go
+++ pkg/events/topics.go
@@ -1,16 +1,8 @@
 package events
 
 const (
-	TopicPacketsDHCP         = "osvbng:events:packets:dhcp"
-	TopicPacketsARP          = "osvbng:events:packets:arp"
-	TopicPacketsPPP          = "osvbng:events:packets:ppp"
-	TopicPacketsL2TP         = "osvbng:events:packets:l2tp"
-	TopicSessionLifecycle    = "osvbng:events:session:lifecycle"
-	TopicAAARequest          = "osvbng:events:aaa:request"
-	TopicAAAResponse         = "osvbng:events:aaa:response"
-	TopicDHCPRelay           = "osvbng:events:dhcp:relay"
-	TopicDHCPResponse        = "osvbng:events:dhcp:response"
-	TopicARPRequest          = "osvbng:events:arp:request"
-	TopicSubscriberState     = "osvbng:events:subscriber:state"
-	TopicEgress              = "osvbng:events:egress"
+	TopicSessionLifecycle = "osvbng:events:session:lifecycle"
+	TopicAAARequest       = "osvbng:events:aaa:request"
+	TopicAAAResponse      = "osvbng:events:aaa:response"
+	TopicEgress           = "osvbng:events:egress"
 )
