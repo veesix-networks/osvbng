fa70ac3c70972ecd81155dcbdc00980800903573
BSpendlove <8687668+BSpendlove@users.noreply.github.com>
2026-01-29 19:41:40 +0000

    dhcp6 provider for extended implementation, added local server implementation based on RFCs

--- plugins/all/all.go
+++ plugins/all/all.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/veesix-networks/osvbng/plugins/auth/all"
 	_ "github.com/veesix-networks/osvbng/plugins/community/all"
 	_ "github.com/veesix-networks/osvbng/plugins/dhcp4/all"
+	_ "github.com/veesix-networks/osvbng/plugins/dhcp6/all"
 	_ "github.com/veesix-networks/osvbng/plugins/exporter/all"
 	_ "github.com/veesix-networks/osvbng/plugins/northbound/all"
 )
