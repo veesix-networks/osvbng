eb8c3c73dc0470f6cc960df680bade873cf5ec53
BSpendlove <8687668+BSpendlove@users.noreply.github.com>
2026-05-22 11:28:22 +0000

    test(ospf6): add JSON parser fixtures and init smoke

---
+++ pkg/handlers/show/protocols/ospf6/init_smoke_test.go
@@ -0,0 +1,9 @@
+// Copyright 2026 The osvbng Authors
+// Licensed under the GNU General Public License v3.0 or later.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package ospf6
+
+import "testing"
+
+func TestInitRegistersWithoutPanic(t *testing.T) {}
